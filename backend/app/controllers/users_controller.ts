7a2e498a164abd4e0180fe938daf4c2a5f1d5d7d
Souhila Yousfi <souhila.yousfi@vif.fr>
2025-04-19 22:39:33 +0200

    Initial commit

---
+++ backend/app/controllers/users_controller.ts
@@ -0,0 +1,10 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import db from '@adonisjs/lucid/services/db'
+
+export default class UsersController {
+  async index({ request }: HttpContext) {
+    const users = await db.query().from('users').select('*')
+
+    return users
+  }
+}
